0b3cca67fb3878ff180fd62d4f12d40ea8ba1f05
PlaidMrdeer <guowenhui28@gmail.com>
2023-09-12 18:01:08 +0800

    第一次提交

---
+++ src/main/java/indi/plaidmrdeer/stellodyssey/Main.java
@@ -0,0 +1,19 @@
+package indi.plaidmrdeer.stellodyssey;
+
+import com.jme3.app.SimpleApplication;
+
+/**
+ * @author PlaidMrdeer
+ */
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        app.start();
+    }
+
+    @Override
+    public void simpleInitApp() {
+    }
+}
+
